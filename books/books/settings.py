100e495443b59ced65c17abf4a448a87830b2874
evgeny <zhenya.leshhinskij@gmail.com>
2024-04-29 17:24:52 +0300

    API

--- books/books/settings.py
+++ books/books/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +77,15 @@ WSGI_APPLICATION = 'books.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default":{
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "books_db",
+        "USER": "books_user",
+        "PASSWORD": "qwerty",
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ books/store/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from store.models import Book
+
+
+class BooksSerializer(ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+
+        
